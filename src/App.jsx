8a8d5b1b6404be375c0cdd7981e09df3a182c4f0
Skierhy <Skierhy@gmail.com>
2022-12-20 15:13:53 -0600

    creado los archivos v2

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
+import { About } from './components/about/About';
+import { Contact } from './components/contact/Contact';
+import { Experience } from './components/experience/Experience';
+import { Footer } from './components/footer/Footer';
+import { Header } from './components/header/Header';
+import { Nav } from './components/nav/Nav';
+import { Portfolio } from './components/portfolio/Portfolio';
+import { Services } from './components/services/Services';
+import { Testimonials } from './components/testimonials/Testimonials';
+
 const App = () => {
 	return (
 		<>
-			<h1>React App</h1>
+			<Header />
+			<Nav />
+			<About />
+			<Experience />
+			<Services />
+			<Portfolio />
+			<Testimonials />
+			<Contact />
+			<Footer />
 		</>
 	);
 };
