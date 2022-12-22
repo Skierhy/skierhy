6f1103ae362be8602ee75157e1dc8b01e6bc5cd4
Skierhy <Skierhy@gmail.com>
2022-12-21 23:13:57 -0600

    mejoras de footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,16 +1,16 @@
-import './footer.css';
 import { FaGithub } from 'react-icons/fa';
 import { BsLinkedin } from 'react-icons/bs';
+import './footer.css';
 
 export const Footer = () => {
 	return (
 		<footer>
-			<a href='#home' className='footer__logo'>
+			<a href='#' className='footer__logo'>
 				Skierhy
 			</a>
 			<ul className='permalinks'>
 				<li>
-					<a href='#home'>Inicio</a>
+					<a href='#'>Inicio</a>
 				</li>
 				<li>
 					<a href='#about'>Sobre mi</a>
@@ -22,12 +22,10 @@ export const Footer = () => {
 					<a href='#portfolio'>Portafolio</a>
 				</li>
 				<li>
-					<a href='#testimonials'>Testimonios</a>
-				</li>
-				<li>
 					<a href='#contact'>Contacto</a>
 				</li>
 			</ul>
+
 			<div className='footer__socials'>
 				<a
 					href='https://www.linkedin.com/in/luis-leonardo-nolasco-najera-skierhy'
